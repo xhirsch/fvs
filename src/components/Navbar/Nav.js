0c3792bb82f136481e56dfb19536929f3c2bd6c2
xhirsch <info@xaverhirsch.de>
2020-02-13 12:29:25 +0100

    fixed sidebar styling

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,55 +1,57 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Hamburger from "./Hamburger";
 
 const Nav = ({}) => {
-  return(
-      <nav>
-        {/* schwarzer Balken top screen */}
-        <div className="bg-black h-2"></div>
-             {/* Navbar*/}
-            <div className="flex items-center font-bold h-10">
-              
-              {/* Burger */}
-              <div className="ml-2">
-                <Hamburger />
-              </div>
-              
-              {/* Navigation links */}
-              <div>
-                <ul className="flex">
-                  <Link to="/ausstellungen" className="ml-4">
-                    <li className="px-3 py-1 hover:bg-white text-xs">Ausstellungen</li>
-                  </Link>
-                  <Link to="/publikationen" className="ml-4">
-                    <li className="px-3 py-1 hover:bg-white text-xs">Publikationen</li>
-                  </Link>
-                  <Link to="/projekte" className="ml-4">
-                    <li className="px-3 py-1 hover:bg-white text-xs">Projekte</li>
-                  </Link>
-                  <Link to="/biographie" className="ml-4">
-                    <li className="px-3 py-1 hover:bg-white text-xs">Biographie</li>
-                  </Link>
-                  <Link to="/impressum" className="ml-4">
-                    <li className="px-3 py-1 hover:bg-white text-xs">Impressum</li>
-                  </Link>
-                  <Link to="/datenschutz" className="ml-4">
-                    <li className="px-3 py-1 hover:bg-white text-xs">Datenschutz</li>
-                  </Link>
-                </ul>
-              </div>
+  return (
+    <nav className="z-100 w-full">
+      {/* schwarzer Balken top screen */}
+      <div className="bg-black h-2"></div>
+      {/* Navbar*/}
+      <div className="flex items-center font-bold h-10">
+        {/* Burger */}
+        <div className="ml-2">
+          <Hamburger />
+        </div>
 
-              {/* Spacer */}
-              <div className="flex-1"/>
+        {/* Navigation links */}
+        <div>
+          <ul className="flex">
+            <Link to="/ausstellungen" className="ml-4">
+              <li className="px-3 py-1 hover:bg-white text-xs">
+                Ausstellungen
+              </li>
+            </Link>
+            <Link to="/publikationen" className="ml-4">
+              <li className="px-3 py-1 hover:bg-white text-xs">
+                Publikationen
+              </li>
+            </Link>
+            <Link to="/projekte" className="ml-4">
+              <li className="px-3 py-1 hover:bg-white text-xs">Projekte</li>
+            </Link>
+            <Link to="/biographie" className="ml-4">
+              <li className="px-3 py-1 hover:bg-white text-xs">Biographie</li>
+            </Link>
+            <Link to="/impressum" className="ml-4">
+              <li className="px-3 py-1 hover:bg-white text-xs">Impressum</li>
+            </Link>
+            <Link to="/datenschutz" className="ml-4">
+              <li className="px-3 py-1 hover:bg-white text-xs">Datenschutz</li>
+            </Link>
+          </ul>
+        </div>
 
-              {/* Name rechts */}
-              <div className="">
-                <p className="px-3 py-1 text-xs">Friedemann von Stockhausen</p>
-              </div>
+        {/* Spacer */}
+        <div className="flex-1" />
 
-            </div>
-      </nav>
+        {/* Name rechts */}
+        <div className="">
+          <p className="px-3 py-1 text-xs">Friedemann von Stockhausen</p>
+        </div>
+      </div>
+    </nav>
   );
-}
- 
+};
+
 export default Nav;
